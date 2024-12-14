c31e44e64b6a995e3fe451ed9dd0fb894d82cb7b
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2024-12-14 12:12:08 -0500

    added typography partial

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 function App() {
   return (
     <>
-      <>Publish your podcasts everywhere.</>
+      <h1 className="text-preset-1">Publish your podcasts everywhere.</h1>
 
-      <>
+      <p className="text-preset-2">
         Upload your audio to Pod with a single click. We’ll then distribute your
         podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and
         more!
-      </>
+      </p>
 
-      <>Request access</>
+      <button className="text-preset-3">Request access</button>
     </>
   );
 }
