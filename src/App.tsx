f5a6452181b3ee2669720cfb6bcf2cb0872a9c1a
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2024-12-14 16:57:31 -0500

    added components partials, changed variables a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
+import Logo from "./components/Logo";
+
 function App() {
   return (
     <>
-      <h1 className="text-preset-1">Publish your podcasts everywhere.</h1>
+      <Logo />
 
-      <p className="text-preset-2">
+      <h1>Publish your podcasts everywhere.</h1>
+
+      <p>
         Upload your audio to Pod with a single click. We’ll then distribute your
         podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and
         more!
       </p>
 
-      <button className="text-preset-3">Request access</button>
+      <form action="" className="form">
+        <input
+          type="email"
+          name="text-field"
+          id="text-field"
+          className="text-field"
+          placeholder="Email Address"
+          aria-label="Enter your email address to request access to our podcasts"
+        />
+        <input type="submit" className="button" value={`Request Access`} />
+      </form>
     </>
   );
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+function Logo() {
+  return (
+    <img src="./images/desktop/logo.svg" alt="Logo of Pod" className="logo" />
+  );
+}
+
+export default Logo;
